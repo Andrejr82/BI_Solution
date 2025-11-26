8224b88ddcbdb24c4b2f780d4cf64ce70efbce64
André Mauricio <79088224+devAndrejr@users.noreply.github.com>
2025-11-26 00:00:44 -0300

    chore: apply cleanup changes to main

--- core/config/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-Script Python para a finalidade de '  init  '.
-"""
-
-# Este arquivo marca o diretório 'core/config' como um pacote Python.
-# O objetivo deste __init__.py é expor as variáveis de configuração
-# do módulo 'config.py' para que possam ser importadas de forma mais limpa
-# de outras partes da aplicação, como rom core.config import DB_SERVER.
